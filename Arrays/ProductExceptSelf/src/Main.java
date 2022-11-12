97415e919e0d60ee4dd8ec5fcd864dfb9804e2f7
ToroEtele <toroetele@gmail.com>
2022-11-12 14:05:44 +0200

    Solve ProductExceptSelf

--- Arrays/ProductExceptSelf/src/Main.java
+++ Arrays/ProductExceptSelf/src/Main.java
@@ -19,7 +19,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-    int[] nums = {1, 2, 3, 4};
-    System.out.println(Arrays.toString(productExceptSelf(nums)));
+    int[] input = {1, 2, 3, 4};
+    System.out.println(Arrays.toString(productExceptSelf(input)));
     }
 }
