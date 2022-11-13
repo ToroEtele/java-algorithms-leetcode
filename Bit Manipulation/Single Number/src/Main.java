43ecedb05a034fadf4f89fd5dfa82d0397e295b1
ToroEtele <toroetele@gmail.com>
2022-11-13 12:00:56 +0200

    Solve SingleNumber

---
+++ Bit Manipulation/Single Number/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static int singleNumber(int[] nums) {
+        int xor=nums[0];
+        for( int i = 1; i<nums.length; i++)
+            xor = xor ^ nums[i];
+        return xor;
+    }
+    public static void main(String[] args) {
+        int[] nums = {2, 2, 1};
+        System.out.println(singleNumber(nums));
+    }
+}
